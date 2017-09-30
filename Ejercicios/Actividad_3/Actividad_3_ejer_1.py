646513a454e79e96c04ef9265aa881e9a97fe940
pmtempone <pmtempone@gmail.com>
2017-09-30 13:19:58 -0300

    Actividad 3

---
+++ Ejercicios/Actividad_3/Actividad_3_ejer_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 11:40:21 2017
+
+@author: pablotempone
+
+Title: Actividad 3 - Ejercicio 1
+"""
+import numpy as np
+import pandas as pd
+import Ejercicios.Actividad_3.bpn as bpn
+
+drugs = pd.read_excel('/Volumes/Disco_SD/redes_neuronales/Ejercicios/Actividad_3/Drug4.xlsx',header=None)
+
+drugs_np = np.array(drugs)
+
+drugs_np_scale = (drugs_np[:,:6]-drugs_np[:,:6].min(axis=0))/(drugs_np[:,:6].max(axis=0)-drugs_np[:,:6].min(axis=0))
+
+P = drugs_np_scale[:,:6].T
+T = drugs_np[:,6]
+
+T_matriz = np.concatenate(([T==0], [T==1], [T==2], [T==3]), axis=0).astype(int)
+
+
+(w_O, b_O, w_S, b_S, ite, errorProm) = bpn.train_w_fijos(P,T_matriz, T, 3 , 0.1, 0.05, 'logsig', 'tansig', 1000, 0.001, True,2)
+
+
---
+++ Ejercicios/Actividad_3/Actividad_3_ejer_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on Sat Sep 30 11:40:21 2017
+
+@author: pablotempone
+
+Title: Actividad 3 - Ejercicio 2
+"""
+import numpy as np
+import pandas as pd
+import Ejercicios.Actividad_3.ImagenPuntos as ip
+
